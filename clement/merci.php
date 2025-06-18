c54d3f312a6f8dfc5dcacf0144599cafbefca75a
Clément Pusterla-Marquette <clement.pusterla-marquette@lecole-ldlc.com>
2025-06-18 13:15:12 +0200

    Delete clement directory

--- clement/merci.php
+++
@@ -1,7 +0,0 @@
-<?php include 'templates/header.php'; ?>
-
-<h2>Merci pour votre réservation !</h2>
-<p>Un e-mail de confirmation vous a été envoyé.</p>
-<p><a href="films.php">Voir d’autres films</a></p>
-
-<?php include 'templates/footer.php'; ?>
